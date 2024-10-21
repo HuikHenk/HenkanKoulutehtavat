a4260b6ac7364c719776dcf07c3776747b17276b
HuikHenk <henrisakarihuikari@gmail.com>
2024-10-21 15:05:30 +0300

    Korjailin juttuja

--- HenriHuikari_Tehtava7B.py
+++ HenriHuikari_Tehtava7B.py
@@ -1,12 +1,7 @@
-# Kysytään käyttäjältä sanaa
 sana = input("Anna sana: ")
 
-# Luodaan tyhjä merkkijono käännetylle sanalle
 kaannetty_sana = ""
 
-# Käydään sana läpi for-silmukalla lopusta alkuun
 for i in range(len(sana)-1, -1, -1):
-    kaannetty_sana += sana[i].lower()  # Muutetaan kirjain pieneksi ja lisätään merkkijonoon
-
-# Tulostetaan käännetty sana
+    kaannetty_sana += sana[i].lower()  
 print("Käännettynä: " + kaannetty_sana)
